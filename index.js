600afc30c7a86a427603a301b90432bc7dd4d58f
Fabricio Paoloni <paolonifabricio@gmail.com>
2023-06-19 11:56:21 +0200

    Project completed at first try :). Improvements will be implemented in a new branch

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: './public/files' })
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let [fileName, fileType, fileSize] = [req.file.originalname, req.file.mimetype, req.file.size];
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  })
+})
